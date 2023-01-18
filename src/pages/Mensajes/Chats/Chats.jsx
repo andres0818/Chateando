bcc4dcf778536d377032b3fd7ea88f4a38f14631
andres0818 <c.andres.perez.h@gmail.com>
2023-01-18 17:25:39 -0500

    maquetado completado

--- src/pages/Mensajes/Chats/Chats.jsx
+++ src/pages/Mensajes/Chats/Chats.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
+import './Chats.scss'
+import emote from '../img/smile.svg'
+import archivos from '../img/paperclip.svg'
+import enviar from '../img/send.svg'
 
-const Chats = ({dataUsuario}) => {
 
-    console.log(dataUsuario.mensajes);
+const Chats = ({ dataUsuario }) => {
+
+  
   return (
-    <div>
-        <div>
+    <div className='chats'>
+      <div className='chats__containerPerfil'>
+        <img className='chats__perfil' src={dataUsuario.perfil} alt="" />
+      </div>
+      <div className='chats__containerMensajes'>
+        <div className='chats__listaMensajes'>
+
+          {dataUsuario.mensajes.map((e) => {
+            console.log(e);
+            return (
+              <div className='chats__mensaje'>
+              <h3 >{e.mensaje}</h3>
+              <p>{e.hour}</p>
+              </div>
+              )
+          })}
         </div>
+        <div className='chats__input'>
+          <img src={emote} alt="" />
+          <img src={archivos} alt="" />
+          <input className='chats__buscador' type="text" />
+          <img src={enviar} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Chats
+/* 
+
+const lengthMensajes = e.mensajes.length - 1
+const infoMensaje = e.mensajes[lengthMensajes]
+ */
--- src/pages/Mensajes/ListaChats/ListaChats.jsx
+++ src/pages/Mensajes/ListaChats/ListaChats.jsx
@@ -2,35 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "./ListaChats.scss"
 
-function ListaChats({data}) {
-  return (
-    
-    <nav>
-    <ul className='listaChats'>
-        {data === null ? <h1>Cargando...</h1> :
-            data.map((e, index) => {
-                console.log(e.mensajes);
-                const infoMensaje = e.mensajes.pop()
-                return (
-                    
-                        <li key={index} className='listaChats__li'>
-                            <Link className='listaChats__link' to={`/mensajes/${e.id}`}>
-                                <img className='listaChats__perfil' src={e.perfil} alt="" />
-                                <div className='listaChats__data'>
-                                    <div className='listaChats__info' >
-                                        <h1> {e.nombre} </h1>
-                                        <p>{infoMensaje.date}</p>
+function ListaChats({ data }) {
+    return (
+
+        <nav>
+            <ul className='listaChats'>
+                {data === null ? <h1>Cargando...</h1> :
+                    data.map((e, index) => {
+                        const lengthMensajes = e.mensajes.length - 1
+                        const infoMensaje = e.mensajes[lengthMensajes]
+                        return (
+
+                            <li key={index} className='listaChats__li'>
+                                <Link className='listaChats__link' to={`/mensajes/${e.id}`}>
+                                    <img className='listaChats__perfil' src={e.perfil} alt="" />
+                                    <div className='listaChats__data'>
+                                        <div className='listaChats__info' >
+                                            <h1> {e.nombre} </h1>
+                                            <p>{infoMensaje.date}</p>
+                                        </div>
+                                        <p>✔✔{infoMensaje.mensaje} </p>
                                     </div>
-                                    <p>✔✔{infoMensaje.mensaje} </p>
-                                </div>
-                            </Link>
-                        </li>
-                    
-                )
-            })}
-    </ul>
-</nav>
-  )
+                                </Link>
+                            </li>
+
+                        )
+                    })}
+            </ul>
+        </nav>
+    )
 }
 
 export default ListaChats
